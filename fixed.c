5ccccca76ac40a06c21e8f3b3684d1c2bfffacd7
skalpovitsh <92952971+skalpovitsh@users.noreply.github.com>
2022-03-22 13:41:57 +0100

    Create fixed.c

---
+++ fixed.c
@@ -0,0 +1,13 @@
+GtkWidget*fix, *btn,*window;
+  //create a window
+    window= gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_widget_set_size_request(window,700,750);
+    gtk_window_set_resizable(GTK_WINDOW(window),0);
+  int x=100,y=200;
+  //creation d'un fixed
+  fix= gtk_fixed_new();
+  //ajouter un widget dans une position (100,200)
+   gtk_fixed_move(GTK_FIXED(fix),btn,x,y);
+   //deplacer un element vers une nouvelle position (40,10)
+  gtk_fixed_put(GTK_FIXED(fix),btn,40,10);
+   gtk_container_add(GTK_CONTAINER(window),fix);
